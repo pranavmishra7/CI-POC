fa1204f596e38511df9c472319996bdb73119c3b
pranavmishra7 <pranav.mishra@hotmail.com>
2016-05-05 11:02:19 +0530

    updated product controller

--- CI/CI/Controllers/CustomersController.cs
+++ CI/CI/Controllers/CustomersController.cs
@@ -17,6 +17,7 @@ namespace CI.Controllers
         // GET: Customers
         public ActionResult Index()
         {
+            Product mdlproduct = new Product();
             return View(db.Customers.ToList());
         }
 
